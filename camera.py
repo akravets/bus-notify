46f2b99cac66add60e52c5f0bbae18c48cea3a66
Alex Kravets <kravets.dev@gmail.com>
2018-12-29 07:19:29 -0500

    removed file check

--- camera.py
+++ camera.py
@@ -10,12 +10,9 @@ def vars():
 
 def read_from_stream():
    Timer(5.0,read_from_stream,[]).start()
-   if(os.path.exists(file_name)):
-       os.remove(file_name)
    vidcap = cv.VideoCapture('rtsp://root:azr26p@192.168.1.25:554/stream1')
    success,image = vidcap.read()
    cv.imwrite(file_name, image)     # save frame as JPEG file     
-   print("test")
 
 vars()
 read_from_stream()
