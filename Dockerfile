bc78f563adc76534e485356bbff4e6bc77657b5b
Alex Kravets <kravets.dev@gmail.com>
2018-12-29 22:58:51 -0500

    added logging

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.0
+FROM python:3.6.8
 
 WORKDIR /camera
 COPY . /camera
--- camera.py
+++ camera.py
@@ -2,9 +2,13 @@ import cv2 as cv
 import threading
 import os
 import os.path
+import logging
 from threading import Timer
 
 COUNT = 0
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def read_from_stream():
    global COUNT
@@ -12,7 +16,7 @@ def read_from_stream():
    vidcap = cv.VideoCapture('rtsp://root:azr26p@192.168.1.25:554/stream1')
    success,image = vidcap.read()
    cv.imwrite("frame%d.jpg" % COUNT, image)     # save frame as JPEG file     
+   logger.info("wrote frame%d.jpg" % COUNT)
    COUNT += 1
-   print(COUNT)
 
 read_from_stream()
