afaea75e3e8f9a0657b4b2ee80ddb987bbaa478f
Alex Kravets <kravets.dev@gmail.com>
2019-01-01 20:24:41 -0500

    work on docker image

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,20 @@
 FROM python:3.6.8
+FROM ubuntu:18.10
 
 WORKDIR /camera
 COPY . /camera
 
 #RUN pip install --trusted-host pypi.python.org -r requirements.txt
-RUN pip install opencv-python
-CMD [ "python", "camera.py" ]
+RUN apt-get update
+RUN apt-get install -y zip unzip python3.6 python3-pip wget
+RUN apt-get install -y libsm6 libxext6 libxrender1
+RUN apt-get update
+
+RUN python3.6 -m pip install pip --upgrade
+RUN pip install opencv-contrib-python
+RUN pip install tensorflow
+
+RUN wget https://github.com/tensorflow/models/archive/master.zip -P /camera
+RUN unzip /camera/master.zip
+
+CMD ["python3", "camera.py"]
--- camera.py
+++ camera.py
@@ -4,19 +4,23 @@ import os
 import os.path
 import logging
 from threading import Timer
+#import subprocess
 
 COUNT = 0
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+#subprocess.call("python", "classify_image.py","--image_file", "bus.jpg")
+
 def read_from_stream():
    global COUNT
    Timer(1.0,read_from_stream,[]).start()
    vidcap = cv.VideoCapture('rtsp://root:azr26p@192.168.1.25:554/stream1')
    success,image = vidcap.read()
-   cv.imwrite("frame%d.jpg" % COUNT, image)     # save frame as JPEG file     
-   logger.info("wrote frame%d.jpg" % COUNT)
+   logger.info("got image")
+   cv.imwrite("frame.jpg", image)     # save frame as JPEG file     
+   os.system("/camera/models-master/tutorials/image/imagenet/classify_image.py --image_file=frame.jpg")
    COUNT += 1
 
 read_from_stream()
